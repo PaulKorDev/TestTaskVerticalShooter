ebe0e539e84eb4b2747da2cf18d16f0619d3013f
PaulKorDev <pauldeveloper@outlook.com>
2024-07-16 16:14:41 -0700

    Squashed commit of the following:
    commit c0f34ee90d83ff4b68d3b39040dde5fc02e7e119
    Author: PaulKorDev <pauldeveloper@outlook.com>
    Date:   Tue Jul 16 16:13:11 2024 -0700
    
        Add Movement for player. Used velocity
    
    commit 5411fe9032256336b5f0a4493f142a3a40a9e183
    Author: PaulKorDev <pauldeveloper@outlook.com>
    Date:   Tue Jul 16 12:59:26 2024 -0700
    
        Import and setup DOTween
    
    commit b435b86af52102b095089b4aba1e989da1fdf495
    Author: PaulKorDev <pauldeveloper@outlook.com>
    Date:   Tue Jul 16 12:52:13 2024 -0700
    
        Created draft of movement

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float _speedMovement;
+
+        private PlayerMovement _movement;
+
+        private void Awake()
+        {
+            var rgb = gameObject.GetComponent<Rigidbody2D>();
+            _movement = new PlayerMovement(rgb);
+        }
+
+        public void FixedUpdate()
+        {
+            _movement.Move(_speedMovement);
+        }
+    }
+}
