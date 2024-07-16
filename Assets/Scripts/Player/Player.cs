b435b86af52102b095089b4aba1e989da1fdf495
PaulKorDev <pauldeveloper@outlook.com>
2024-07-16 12:52:13 -0700

    Created draft of movement

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float _speedMovement;
+
+        private PlayerMovement _movement;
+
+        private void Awake()
+        {
+            _movement = new PlayerMovement(_speedMovement, transform);
+        }
+
+        public void Update()
+        {
+            _movement.Move();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerMovement
+    {
+        private float _speedMovement;
+        private Transform _player;
+
+        public PlayerMovement(float speed, Transform player)
+        {
+            _speedMovement = speed;
+            _player = player;
+        }
+
+        public void Move()
+        {
+          
+        }
+
+    }
+}
