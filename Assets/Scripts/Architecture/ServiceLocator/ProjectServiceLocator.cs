04e5d5b889d08c72eb8d1428f9c37595b2307681
PaulKorDev <pauldeveloper@outlook.com>
2024-07-16 12:29:41 -0700

    Add Project- and Scene- ServiceLocator, also adjsut hierarhy on scene

---
+++ Assets/Scripts/Architecture/ServiceLocator/ProjectServiceLocator.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Scripts.Architecture.ServiceLocator
+{
+    public class ProjectServiceLocator 
+    {
+        public void RegisterAllServices()
+        {
+            //here register services
+        }
+
+        //Unregister don't need, because here are permanent services
+    }
+}
