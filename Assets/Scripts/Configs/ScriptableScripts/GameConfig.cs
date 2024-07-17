0c62e579a68794e68690f3c944c752c2da5e18aa
PaulKorDev <pauldeveloper@outlook.com>
2024-07-16 17:39:00 -0700

    Remove useless GameplayLoadState, because there is only 1 scene. Register configs to servicelocator

--- Assets/Scripts/Configs/ScriptableScripts/GameConfig.cs
+++ Assets/Scripts/Configs/ScriptableScripts/GameConfig.cs
@@ -1,7 +1,8 @@
+using Assets.Scripts.Architecture.ServiceLocator;
 using UnityEngine;
 
 [CreateAssetMenu (fileName = "GameConfig", menuName = "ScriptableObjects/GameConfig")]
-public class GameConfig : ScriptableObject
+public class GameConfig : ScriptableObject, IService
 {
     public PlayerAttackConfig PlayerAttackConfig; 
     public EnemyFactoryConfig EnemyFactoryConfig;
