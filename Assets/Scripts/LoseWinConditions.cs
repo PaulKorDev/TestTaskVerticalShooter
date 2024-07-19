01ebf8315e6fe5afcbc0f2e0383e239d52ae1a16
PaulKorDev <pauldeveloper@outlook.com>
2024-07-19 05:41:54 -0700

    Remove Debug.Log

--- Assets/Scripts/LoseWinConditions.cs
+++ Assets/Scripts/LoseWinConditions.cs
@@ -37,7 +37,6 @@ namespace Assets.Scripts
             var leftEnemies = ServiceLocator.Get<EnemyObjectPool>().CountActiveObjects();
             var playerHP = ServiceLocator.Get<Player.Player>().GetHP();
             var allEnemiesAlreadySpawned = _enemySpawner.isAllUnitSpawned();
-            Debug.Log($"lEFT: {leftEnemies}, HP: {playerHP}, spawned: {allEnemiesAlreadySpawned}");
             if (allEnemiesAlreadySpawned && playerHP != 0 && leftEnemies == 0)
             {
                 _eventBus.OnPlayerWon.Trigger();
